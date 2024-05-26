6210c3bd361e09dd7cedc8e20783ec2548310111
nekokujira24wase <120014921+nekokujira24wase@users.noreply.github.com>
2024-05-26 15:48:16 +0900

    corsのインポートを方法を変更

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,10 @@
 import express, { Request, Response } from "express";
-// import cors from 'cors';
-const cors = require('cors');
-// const json = require('json');
+import cors from 'cors';
 
 const app = express();
 const port = 4000;
 
 app.use(cors());
-// app.use(json());
 
 app.get('/api/greeting', (req: Request, res: Response) => {
   res.json({ message: 'React | TypeScript | Express | Axios' });
